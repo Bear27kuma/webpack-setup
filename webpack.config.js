6e6af9a9de7117da26ddf71d0a0d25dad8ce6f12
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-11 21:29:21 +0900

    Sassファイルをバンドルする

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 const path = require('path');
 
 module.exports = {
+  // バンドルするモード
   mode: 'development',
   devtool: false,
+  // 全てのファイルの基準となるファイル
   entry: { app: './src/app.js' },
+  // 複数の指定も可能
   // entry: {
   //   app: './src/app.js',
   //   sub: './src/sub.js'
   // }
+  // 出力先フォルダとファイル名
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // 対象となる拡張子
+        test: /\.scss$/,
+        // どのloaderを使用するか
+        use: [
+          // 下から実行されるため、最初に実行したいものを末尾に記述
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   }
 };
